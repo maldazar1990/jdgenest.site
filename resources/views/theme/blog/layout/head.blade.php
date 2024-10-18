29109634f18718a3bb10ecef1b1f822ddb531b0e
jdgenest <jdgenest19@gmail.com>
2024-10-17 23:11:30 -0400

    correction

--- resources/views/theme/blog/layout/head.blade.php
+++ resources/views/theme/blog/layout/head.blade.php
@@ -17,8 +17,8 @@
     @if(isset($SEOData))
         {!! seo($SEOData) !!}
     @endif
-    <link id="theme-style" rel="stylesheet" href="{{asset("front/css/app.min.css")}}?v={{microtime()}}">
-    <script src="{{asset("front/js/app.min.js")}}" defer></script>
+    <link id="theme-style" rel="stylesheet" href="{{asset('front/css/app.css')}}?v={{microtime()}}">
+    <script src="{{asset('front/js/app.js')}}" defer></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css" integrity="sha512-q3eWabyZPc1XTCmF+8/LuE1ozpg5xxn7iO89yfSOd5/oKvyqLngoNGsx8jq92Y8eXJ/IRxQbEC+FGSYxtk2oiw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/js/all.min.js" integrity="sha512-LW9+kKj/cBGHqnI4ok24dUWNR/e8sUD8RLzak1mNw5Ja2JYCmTXJTF5VpgFSw+VoBfpMvPScCo2DnKTIUjrzYw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </head>
