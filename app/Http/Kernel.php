3b75f81d9a323ab3b734512544599e858d75e371
jdgenest <jdgenest19@gmail.com>
2024-10-24 12:09:49 -0400

    correction form post

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,7 +3,7 @@
 namespace App\Http;
 
 use App\Http\Middleware\Cors;
-use Illuminate\Console\Scheduling\Schedule;
+use App\Http\Middleware\RedirectToNonWWWMiddleware;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -38,7 +38,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\RedirectToNonWwwMiddleware::class,
+            RedirectToNonWWWMiddleware::class,
             Cors::class,
 
 
