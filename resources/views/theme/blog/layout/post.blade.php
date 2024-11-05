6d494605d7c8984de59706da54cb6e7eac0d44b2
jdgenest <jdgenest19@gmail.com>
2024-11-05 16:49:44 -0500

    correction ratio image

--- resources/views/theme/blog/layout/post.blade.php
+++ resources/views/theme/blog/layout/post.blade.php
@@ -24,7 +24,7 @@
 <div class="col-12 col-md-6 col-lg-6 mb-5 ">
     <div class="card rounded-0 border-0 shadow-sm eq-height shadow-animation" >
         <div class="position-relative">
-            @include("theme.blog.layout.image", ['image' => $post->image,"class" => "img-fluid","size"=>"small"])
+            @include("theme.blog.layout.image", ['image' => $post->image,"class" => "img-fluid","size"=>"medium"])
         </div>
         <div class="card-body pb-4">
             <h3 class="card-title mb-1"><a class="text-link" href="{{route("post",$post->slug)}}">{{$post->title}}</a></h3>
