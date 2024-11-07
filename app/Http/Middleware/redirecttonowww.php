637ddd5eee5c37a7bc7497ba412ec3ceb6cc9b85
jdgenest <jdgenest19@gmail.com>
2024-11-07 13:50:32 -0500

    crrection

--- app/Http/Middleware/redirecttonowww.php
+++ app/Http/Middleware/redirecttonowww.php
@@ -4,8 +4,9 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redirect;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Redirect;
+
 
 class redirecttonowww
 {
